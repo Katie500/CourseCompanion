dc92ec8e1599f3a749fc184843a0f6c4fba8fa20
Yvonne Ng <yvonneng0405@gmail.com>
2023-04-03 21:08:53 -0600

    Added plus button functionality with ICourseService

---
+++ Shared/ICourseService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CourseCompanion.Shared.Model;
+
+namespace CourseCompanion.Shared.Services
+{
+    public interface ICourseService
+    {
+        event Action<CourseDetails> OnCourseAdd;
+        void SendCourse(CourseDetails courseAdded);
+    }
+
+    public class CourseService : ICourseService
+    {
+        public event Action<CourseDetails> OnCourseAdd;
+
+        public void SendCourse(CourseDetails courseAdded)
+        {
+            OnCourseAdd?.Invoke(courseAdded);
+        }
+    }
+}
+

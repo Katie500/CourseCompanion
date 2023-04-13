5115fcfa75611341fd9d49fc131c0e380ddbc79c
muba-shir <95720235+muba-shir@users.noreply.github.com>
2023-04-13 14:37:24 -0600

    Updating shared

---
+++ Shared/Services/ICourseService.cs
@@ -0,0 +1,28 @@
+﻿/*
+ Service message layout referenced from:
+https://jasonwatmore.com/post/2020/07/30/aspnet-core-blazor-webassembly-communication-between-components
+ */
+
+using System;
+using CourseCompanion.Shared.Model;
+
+namespace CourseCompanion.Shared.Services
+{
+    public interface ICourseService
+    {
+        event Action<CourseDetails> OnCourseEvent;
+        void CourseEvent(CourseDetails course);
+    }
+
+    public class CourseService : ICourseService
+    {
+        public event Action<CourseDetails> OnCourseEvent;
+
+        public void CourseEvent(CourseDetails courseAdded)
+        {
+            OnCourseEvent?.Invoke(courseAdded);
+        }
+
+    }
+}
+
